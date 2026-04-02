5c8c9a581f0f5eaa52c776bc339c1fdb602a1d4b
Riza5251 <rizaqureshi5251@gmail.com>
2026-04-02 18:17:32 +0530

    commit

--- src/main/java/project/HackHustle/controller/SubjectController.java
+++ src/main/java/project/HackHustle/controller/SubjectController.java
@@ -9,7 +9,7 @@ import project.HackHustle.dto.SubjectDto;
 import project.HackHustle.service.SubjectService;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/subjects")
 @RequiredArgsConstructor
